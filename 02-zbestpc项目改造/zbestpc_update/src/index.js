9a30b3498153311ddf83e189bffa940252e0c22e
王添瑜 <11081572+tcwty123@user.noreply.gitee.com>
2024-03-17 16:45:26 +0800

    改造bestPC-首页移植-htmlcssjs图片打包

--- 02-zbestpc项目改造/zbestpc_update/src/index.js
+++ 02-zbestpc项目改造/zbestpc_update/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello')
+import './css/index.css'
+import './css/public.css'
+
+// import './js/jquery-1.12.4.min'
+// import './js/jquery.flexslider-min'
+import 'jquery'
+import 'flexslider'
+import './js/public'
+import './js/nav'
--- 02-zbestpc项目改造/zbestpc_update/webpack.config.js
+++ 02-zbestpc项目改造/zbestpc_update/webpack.config.js
@@ -1,10 +1,45 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ProvidePlugin } = require('webpack')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'js/[name].js',
     path: path.resolve(__dirname, './dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // 打包文件
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // webpack自带
+        type: 'asset',
+        // 如果图片小于8kb，就会被转换成base64
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        // 大于8kb的图片会被打包到images文件夹下
+        generator: {
+          filename: 'images/[name].[hash:6][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 }
