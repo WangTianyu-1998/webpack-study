99ec91f0c33053214bb253f99571db7cda50517c
王添瑜 <11081572+tcwty123@user.noreply.gitee.com>
2024-03-23 18:39:11 +0800

    改造bestPC-webpack多配置文件改造

--- 02-zbestpc项目改造/zbestpc_update/build/webpack.config.js
+++ 02-zbestpc项目改造/zbestpc_update/build/webpack.config.js
@@ -16,12 +16,12 @@ module.exports = {
   mode: 'development',
   // 入口
   entry: {
-    index: './src/index.js',
-    login: './src/login.js',
+    index: path.resolve(__dirname, '../src/index.js'),
+    login: path.resolve(__dirname, '../src/login.js'),
   },
   output: {
     filename: 'js/[name].js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   devServer: {
     static: {
@@ -126,8 +126,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, './src/img'),
-          to: path.resolve(__dirname, './dist/img'),
+          from: path.resolve(__dirname, '../src/img'),
+          to: path.resolve(__dirname, '../dist/img'),
         },
       ],
     }),
