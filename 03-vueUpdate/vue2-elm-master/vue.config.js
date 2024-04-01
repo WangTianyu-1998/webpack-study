45b5d4417ee3f0c4576ff06955ead1f4e252aadd
王添瑜 <11081572+tcwty123@user.noreply.gitee.com>
2024-04-01 22:28:15 +0800

    项目打包构建优化-构建速度和体积

--- 03-vueUpdate/vue2-elm-master/vue.config.js
+++ 03-vueUpdate/vue2-elm-master/vue.config.js
@@ -1,7 +1,19 @@
 const path = require("path");
-// vue.config.js
+// 构建速度
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+// 打包体积
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+// SpeedMeasurePlugin 用于测量webpack构建时间
+const smp = new SpeedMeasurePlugin({
+  // 为true就关闭功能  process.env.MEASURE(文档推荐的方式) 环境变量 可以在package.json中配置
+  disable: !(process.env.MEASURE === "true"),
+  // 输出结果的格式
+  outputFormat: "humanVerbose",
+});
+
 module.exports = {
-  configureWebpack: {
+  configureWebpack: smp.wrap({
     resolve: {
       alias: {
         src: path.resolve(__dirname, "./src"),
@@ -9,5 +21,6 @@ module.exports = {
         components: path.resolve(__dirname, "./src/components"),
       },
     },
-  },
+    plugins: [new BundleAnalyzerPlugin()],
+  }),
 };
